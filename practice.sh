00148b51fbbee5777f5f953a6cba77f7fb4753e1
Ubuntu <ubuntu@ip-172-31-55-147.ec2.internal>
2023-03-20 23:53:01 +0000

    adding inspo msgs

--- practice.sh
+++ practice.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Author : Nana
 
-echo "just for shegey reason"
+
 echo "we shall strive hard to be devops engineers"
 echo "this is just for practing"
+echo "devops is the ish meeeeeehn"
+echo "we go make am"
 
