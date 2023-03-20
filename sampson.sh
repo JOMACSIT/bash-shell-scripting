789c36b81852e881e02c49c5220cfeb5418fd320
Sampson Kotey Djanie <sampson.kotey.djanie@gmail.com>
2023-03-20 14:25:24 +0000

    adds my assignment solution

---
+++ sampson.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+#Author: Sampson
+
+touch b2.sh bashass.sh bash.sh black.mp3 bubu.avi dame.mov
+david.flac dev.log dino.log edochie.jpg efe.log games.avi
+hunger.mov mapple.jnp naija.jpg nana.mov peace.py pete.png python.py sh
+
+mkdir music images videos
+
+mv *.mp3 *.flac music
+
+mv *.jpg *.png images
+
+mv *.avi *.mov videos
+
+rm *.log
+
+tree
