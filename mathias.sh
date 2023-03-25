fb81d2407bfb7b477e9a7614f2c3341512d33e00
Mat-94 <mathiasmahami@gmail.com>
2023-03-25 00:13:56 +0000

    adds my bash assignment solution

---
+++ mathias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: mathias
+touch b2.sh bashass.sh bash.sh games.avi bubu.avi dame.mov maapple.jnp
+peace.py sheriff.flac zanku.mp3 pete.png edochie.jpg python.py dino.log
+naija.jpg efe.log 
+
+mkdir music images videos
+mv *.mp3 *.flac music
+mv *.jpg *.png image
+mv *.avi *.mov videos
+rm *.log
+tree
