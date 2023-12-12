76de0223236ddc8edd4589e2377c572b4733a636
Gitblix <ben.p.tawiah@gmail.com>
2023-12-12 10:09:09 +0000

    today looks good details

--- ben.sh
+++ ben.sh
@@ -3,8 +3,7 @@
 #Date: 12/12/2023
 
 touch AB.sh AC.mp3 BC.mp3 ACD.flac ABC.jpg ZAK.avi ZAS.mov KOI.avi WALE.mp3 STONE.mp3
-DEV.log NANA.avi VAN.log BOO.log COO.log DOO.log YOO.mp3 YELL.flac KON.tf MAD.sh black.mp3 door.png dat.png ewa.jpg
-
+DEV.log NANA.avi VAN.log BOO.log COO.log DOO.log YOO.mp3 YELL.flac KON.tf MAD.sh black.mp3 door.png dat.png ewa.jpg pass.jpg
 mkdir music images videos
 
 mv *.mp3 *.flac music
